edea3813b619e428130d24a3c1885fe18aa787b2
himamsab <imamsab@gmail.com>
2023-12-28 19:20:14 +0530

    nn

--- demo/src/main/java/com/example/demo/Target.java
+++ demo/src/main/java/com/example/demo/Target.java
@@ -1,9 +1,9 @@
 package com.example.demo;
-<<<<<<< HEAD
+
 //hjkbhvib
-=======
+
 //hhhgggg
->>>>>>> f9b2f0028c5881d2b50ec1f099f2ce0a0b1f18c3
+f9b2f0028c5881d2b50ec1f099f2ce0a0b1f18c3
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 //ou
@@ -30,5 +30,6 @@ public class Target {
 		System.out.println("work started");
 		dp.save();
 		System.out.println("work ended");
+/ended
 	}
 }
